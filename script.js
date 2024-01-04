207d0644b1c1e69a4da8561e7733e7858bacfa19
manuhatho <amateur9525@gmail.com>
2024-01-04 10:44:09 +0100

    v

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function menuBar(){
+    const header = document.getElementById("header");
+    const logo = document.querySelector(".logo");
     const menu = document.querySelector(".menu");
     const  navBlend = document.querySelector(".navBlend");
     const nav = document.querySelector(".nav2");
@@ -10,21 +12,28 @@ function menuBar(){
         menu.classList.toggle("active");
         nav.classList.toggle("active");
         navBlend.classList.toggle("active");
+        header.classList.add("hide");
+        logo.classList.toggle("hide");
     })
    
 }
 menuBar();
 
 function out(){
+    const header = document.getElementById("header");
+    const logo = document.querySelector(".logo");
     const  navBlend = document.querySelector(".navBlend");
     const menu = document.querySelector(".menu");
     const nav = document.querySelector(".nav2");
     const html = document.querySelector("html");
     navBlend.addEventListener("click",()=>{
+        header.classList.add("hide");
         html.classList.toggle("active"); 
         menu.classList.toggle("active");
         nav.classList.toggle("active");
         navBlend.classList.toggle("active");
+        header.classList.add("hide");
+        logo.classList.toggle("hide");
     })
 }
 out();
